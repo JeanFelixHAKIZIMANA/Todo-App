7588ec7ce602cb5c6962106934ac733b66be328e
JeanFelixHAKIZIMANA <hakfelix90gmail.com>
2024-07-15 12:21:18 +0200

    initialized to do app template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+export default function App () {
+  return 
+  <form className='new-item-form'>
+    <div className='form-row'> 
+      <label className='' htmlFor='item'>New Item</label>
+      <input type='text' id='item' />
+    </div>
+    <button className='btn'>Add</button>
+  </form>
+}
